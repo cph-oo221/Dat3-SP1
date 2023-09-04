6f256d2ec16e0d27aa8592231fdbb811037b03cf
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:57:22 +0200

    Update PersonDetail.java

--- src/main/java/dat/entities/PersonDetail.java
+++ src/main/java/dat/entities/PersonDetail.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 @Entity
 @Setter
 @Getter
@@ -31,4 +35,40 @@ public class PersonDetail
 
     @ManyToOne
     private Address address;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    Set<Phone> phoneSet = new HashSet<>();
+
+    public void addPhone(Phone phone)
+    {
+        this.phoneSet.add(phone);
+
+        if (phone != null)
+        {
+            phone.setPersonDetail(this);
+        }
+    }
+    
+    @PrePersist
+    public void verifyEmail()
+    {
+        if (!validEmail())
+        {
+            throw new IllegalArgumentException("The Email: " + getEmail() + " is not valid");
+        }
+    }
+
+    @PreUpdate
+    public void verifyUpdatedEmail()
+    {
+        if (!validEmail())
+        {
+            throw new IllegalArgumentException("The Email: " + getEmail() + " is not valid");
+        }
+    }
+
+    private boolean validEmail()
+    {
+        return Pattern.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", getEmail());
+    }
 }
