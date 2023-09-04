379a06e1ed1d6ef542eb8995ae531474b1f137d2
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:30:27 +0200

    Create Zip.java

---
+++ src/main/java/dat/entities/Zip.java
@@ -0,0 +1,27 @@
+package dat.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Zip
+{
+    @Id
+    @Column(name = "z_id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(length = 45)
+    private String city;
+
+    @Column(length = 45)
+    private String region;
+
+    @Column(length = 45)
+    private String municipality;
+}
