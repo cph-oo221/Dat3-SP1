7ff7cd113e8050fb9fd08ea2533e9ae96699333c
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:28:54 +0200

    Create Address.java

---
+++ src/main/java/dat/model/Address.java
@@ -0,0 +1,44 @@
+package dat.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+public class Address
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "a_id")
+    private Integer id;
+
+    private String street;
+
+    private String number;
+
+    @ManyToOne
+    @JoinColumn(name = "z_id")
+    private Zip zip;
+
+    @OneToMany
+    private PersonDetail personDetail;
+
+
+    public Address(String street, String number, Zip zip)
+    {
+        this.street = street;
+        this.number = number;
+        this.zip = zip;
+    }
+
+    public void addZip(Zip zip)
+    {
+        this.zip = zip;
+    }
+}
