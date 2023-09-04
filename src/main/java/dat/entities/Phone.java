96e09d4027bd8862b3452ab2fccf4abd8a699297
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:30:21 +0200

    Create Phone.java

---
+++ src/main/java/dat/entities/Phone.java
@@ -0,0 +1,34 @@
+package dat.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+public class Phone
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private Integer id;
+
+    private Integer number;
+
+    private Type type;
+
+    @ManyToOne
+    private PersonDetail personDetail;
+
+    public Phone(Integer number, Type type)
+    {
+        this.number = number;
+        this.type = type;
+    }
+
+}
