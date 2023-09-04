c462fa2c30bfbc7a6a984aeb8ce10448b8df63ca
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:34:38 +0200

    Update Person.java

--- src/main/java/dat/entities/Person.java
+++ src/main/java/dat/entities/Person.java
@@ -25,9 +25,22 @@ public class Person
     @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
     private Set<Interests> interests = new HashSet<>();
 
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
+    private PersonDetail personDetail;
+
     public Person(String name, Date birthdate)
     {
         this.name = name;
         this.birthdate = birthdate;
+    }
+    
+    public void addPersonDetail(PersonDetail personDetail)
+    {
+        this.personDetail = personDetail;
+
+        if (personDetail != null)
+        {
+            personDetail.setPerson(this);
+        }
     }
 }
