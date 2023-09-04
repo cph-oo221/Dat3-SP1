6b3ab639d651a9dc426d63d9e208e957e0528807
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-04 18:30:19 +0200

    Create PersonDetail.java

---
+++ src/main/java/dat/entities/PersonDetail.java
@@ -0,0 +1,34 @@
+package dat.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+public class PersonDetail
+{
+    @Id
+    @MapsId
+    private Integer id;
+
+    @Column(length = 45)
+    private String surname;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 25)
+    private String password;
+
+//    @OneToOne
+//    private Person person;
+
+    @ManyToOne
+    private Address address;
+}
