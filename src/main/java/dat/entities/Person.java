ffd2798cf96608e5859f7e7ae14c4855e0b360fb
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-05 13:27:40 +0200

    Update Person.java

--- src/main/java/dat/entities/Person.java
+++ src/main/java/dat/entities/Person.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,14 +22,14 @@ public class Person
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer p_id;
     private String name;
-    private Date birthdate;
+    private LocalDate birthdate;
     @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
     private Set<Interests> interests = new HashSet<>();
 
     @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
     private PersonDetail personDetail;
 
-    public Person(String name, Date birthdate)
+    public Person(String name, LocalDate birthdate)
     {
         this.name = name;
         this.birthdate = birthdate;
