4f200ed603980726591beca318bf76fd52e09234
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-05 13:27:42 +0200

    Update MainTest.java

--- src/test/java/dat/MainTest.java
+++ src/test/java/dat/MainTest.java
@@ -1,12 +1,19 @@
 package dat;
 
 import dat.config.HibernateConfig;
+import dat.dao.HobbyDAO;
+import dat.dao.PersonDAO;
 import dat.dao.PhoneDAO;
+import dat.entities.*;
 import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,11 +22,15 @@ class MainTest
 {
 
     private PhoneDAO phoneDAO;
+    private HobbyDAO hobbyDAO;
+    private PersonDAO personDAO;
     private EntityManagerFactory emf;
 
     @BeforeAll
     void setUp()
     {
+        phoneDAO = PhoneDAO.getInstance(emf);
+        hobbyDAO = HobbyDAO.getInstance(emf);
         emf = HibernateConfig.getEntityManagerFactoryConfig("hobbiestest", "create");
     }
 
@@ -45,20 +56,29 @@ class MainTest
     void getAllPhoneNumbersForPerson()
     {
         // US - 2
-        // TODO: Write test
+        List<Phone> phoneList = phoneDAO.getAllNumbersByPerson(new Person());
+        int expected = 2;
+        assertEquals(expected, phoneList.size());
     }
 
     @Test
     void getAllPersonsByHobby()
     {
         // US - 3
-        // TODO: Write test
+        Hobby hobby = hobbyDAO.find(1);
+        List<Person> hobbies = hobbyDAO.getAllPersonsByHobby(hobby);
+        int expected = 2;
+        assertEquals(expected, hobbies.size());
     }
 
     @Test
     void getNumberOfPeopleByHobby()
     {
-        // TODO: Write test
+        // US - 4
+        Hobby hobby = hobbyDAO.find(1);
+        hobbyDAO.getHobbyCount(hobby);
+        int expected = 2;
+        assertEquals(expected, hobbyDAO.getHobbyCount(hobby));
     }
 
     @Test
