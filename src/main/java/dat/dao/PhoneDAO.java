ae8be7051ac554035f1a0cf19f7d69b313ee21ad
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-05 11:45:00 +0200

    Update PhoneDAO.java

--- src/main/java/dat/dao/PhoneDAO.java
+++ src/main/java/dat/dao/PhoneDAO.java
@@ -1,6 +1,12 @@
 package dat.dao;
 
+import dat.entities.Hobby;
+import dat.entities.Person;
+import dat.entities.Phone;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
 
 public class PhoneDAO
 {
@@ -20,5 +26,26 @@ public class PhoneDAO
         }
         this.emf = emf;
         return instance;
+    }
+
+
+    // US 2
+    public List<Phone> getAllPhones(Person person)
+    {
+        try(EntityManager em = emf.createEntityManager())
+        {
+            return em.createQuery("SELECT p.personDetail.phoneSet from Phone p where p.personDetail.person.id = :id", Phone.class)
+                    .setParameter("id", person.getP_id()).getResultList();
+        }
+    }
+
+
+    // US 4
+    public String getPhoneByHobby(Hobby hobby)
+    {
+        try(EntityManager em = emf.createEntityManager())
+        {
+            return em.createQuery(
+        }
     }
 }
