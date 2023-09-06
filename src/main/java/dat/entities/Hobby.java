7ed52a87273bcfe5bf65f8fe342902d9aa89217e
kotteletfisk <ostekugle@gmail.com>
2023-09-06 10:58:23 +0200

    Fixed emf issues in test

--- src/main/java/dat/entities/Hobby.java
+++ src/main/java/dat/entities/Hobby.java
@@ -19,15 +19,16 @@ public class Hobby
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer h_id;
-    private String link;
+    private String name;
+    private String wikilink;
     private String category;
     private String type;
     @OneToMany(mappedBy = "hobby", cascade = CascadeType.ALL)
     private Set<Interests> interests = new HashSet<>();
 
-    public Hobby(String link, String category, String type)
+    public Hobby(String wikilink, String category, String type)
     {
-        this.link = link;
+        this.wikilink = wikilink;
         this.category = category;
         this.type = type;
     }
